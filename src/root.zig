7efb5c9b0098f543b7eba50eea5da0148dea646b
Tpaefawzen <100011199+Tpaefawzen@users.noreply.github.com>
2024-08-30 07:15:04 +0900

    Created root.factory

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,26 @@
-const std = @import("std");
-const testing = std.testing;
+//! Programming language Dis implementation
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+/// UintT types unsigned integer.
+/// Create a virtual machine of Dis.
+pub fn factory(UintT: type, base: UintT, digit: UintT) !type {
+    const INT_END_ = try @import("std.math").powi(UintT, base, digit);
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+    return struct {
+        const Self = @This();
+
+	/// Data. Actual valid integer shall be integer such that 0<=n<=INT_MAX.
+	pub const INT_END = INT_END_;
+	pub const INT_MAX = INT_END_ - 1;
+
+	/// Registers
+	pub var a: UintT = 0;
+	pub var c: UintT = undefined;
+	pub var d: UintT = undefined;
+
+	/// Memory
+	pub var mem: [0:Self.INT_END]UintT = .{0};
+
+	// TODO
+    }; // struct
+} // pub fn factory
+
