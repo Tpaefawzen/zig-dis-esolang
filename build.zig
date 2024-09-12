b4ea2d7cea8d08f1b8113fa07aaf78c0e126b2e4
Tpaefawzen <100011199+Tpaefawzen@users.noreply.github.com>
2024-09-12 17:49:58 +0900

    Added tests.zig as central test file

--- build.zig
+++ build.zig
@@ -30,7 +30,7 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
-        .name = "dis-esolang.zig",
+        .name = "dis-esolang",
         .root_source_file = b.path("src/main-cli.zig"),
         .target = target,
         .optimize = optimize,
@@ -67,7 +67,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
